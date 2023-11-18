ae510788f0b2b53705973231db817ca1bd024582
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2023-11-18 16:36:32 -0500

    exercise 4

--- world.js
+++ world.js
@@ -2,7 +2,7 @@ window.onload = () => {
     const countryInput = document.getElementById("country");
     const lookUpBtn = document.getElementById("lookup");
     const result = document.getElementById("result");
-    
+
     lookUpBtn.addEventListener("click" , async () => {
         const response = await (await fetch(`world.php?name=${countryInput.value}`)).text();
         console.log(response)
