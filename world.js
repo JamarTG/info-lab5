1904b43045d5dae77352f8182e7b7d02bee67236
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2023-11-19 13:57:32 -0500

    exercise 5

--- world.js
+++ world.js
@@ -1,10 +1,16 @@
 window.onload = () => {
-    const countryInput = document.getElementById("country");
-    const lookUpBtn = document.getElementById("lookup");
+    const searchInput = document.getElementById("search");
+    const countryLookUpBtn = document.getElementById("country-lookup");
+    const cityLookUpBtn = document.getElementById("city-lookup");
     const result = document.getElementById("result");
 
-    lookUpBtn.addEventListener("click" , async () => {
-        const response = await (await fetch(`world.php?name=${countryInput.value}`)).text();
+    countryLookUpBtn.addEventListener("click" , async () => {
+        const response = await (await fetch(`world.php?country=${searchInput.value}`)).text();
+        console.log(response)
+        result.innerHTML = response;
+    })
+    cityLookUpBtn.addEventListener("click" , async () => {
+        const response = await (await fetch(`world.php?city=${searchInput.value}`)).text();
         console.log(response)
         result.innerHTML = response;
     })
--- world.php
+++ world.php
@@ -3,31 +3,54 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
-$countryName = $_GET['name'] ?? '';
-
+$country = $_GET['country'] ?? '';
+$city = $_GET['city'] ?? '';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$countryName%'");
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$results = [];
+
+if ($country != '') {
+    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $stmt->execute(); 
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+} else if ($city != '') {
+    $stmt = $conn->prepare("SELECT * FROM cities WHERE name LIKE '%$city%'");
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+}
 
 ?>
 <table>
   <thead>
     <tr>
+    <?php if(!empty($country) ) {?>
       <th>Country Name</th>
       <th>Continent</th>
       <th>Independence Year</th>
       <th>Head of State</th>
+    <?php } elseif(!empty($city)) {?>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
+    <?php }?>
     </tr>
   </thead>
   <tbody>
     <tr>
       <?php foreach ($results as $row): ?>
         <tr>
-          <td><?= !empty($row['name']) ? $row['name'] : '-' ?></td>
-          <td><?= !empty($row['continent']) ? $row['continent'] : '-' ?></td>
-          <td><?= !empty($row['independence_year']) ? $row['independence_year'] : '-' ?></td>
-          <td><?= !empty($row['head_of_state']) ? $row['head_of_state'] : '-' ?></td>
+          <?php if(!empty($country)){?>
+            <td><?= !empty($row['name']) ? $row['name'] : '-' ?></td>
+            <td><?= !empty($row['continent']) ? $row['continent'] : '-' ?></td>
+            <td><?= !empty($row['independence_year']) ? $row['independence_year'] : '-' ?></td>
+            <td><?= !empty($row['head_of_state']) ? $row['head_of_state'] : '-' ?></td>
+          <?php } else if(!empty($city)){ ?>
+            <td><?= !empty($row['name']) ? $row['name'] : '-' ?></td>
+            <td><?= !empty($row['district']) ? $row['district'] : '-' ?></td>
+            <td><?= !empty($row['population']) ? $row['population'] : '-' ?></td>
+          <?php }?>
+          
         </tr>
       <?php endforeach; ?>
     </tr>
